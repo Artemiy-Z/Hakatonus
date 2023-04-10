3e4fef00eb21243cedee12f3a1ddcd00fbf6ba35
vladg <vladgro528@gmail.com>
2023-04-10 23:29:02 +0500

    добавил три активность, если что класы можно удалить потом

---
+++ app/src/main/java/com/example/hakatonus/CodeSMS.java
@@ -0,0 +1,14 @@
+package com.example.hakatonus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodeSMS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_sms);
+    }
+}
