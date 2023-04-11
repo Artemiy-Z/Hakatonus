74e618449c92592722478813965215c99ad65dd3
vladg <vladgro528@gmail.com>
2023-04-11 12:55:55 +0500

    сделал интро

--- app/src/main/java/com/example/hakatonus/IntroActivity.java
+++ app/src/main/java/com/example/hakatonus/IntroActivity.java
@@ -2,54 +2,105 @@ package com.example.hakatonus;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
 public class IntroActivity extends FragmentActivity {
 
+    private AppCompatButton prevB;
+    private AppCompatButton nextB;
+    private AppCompatButton btn_passenger;
+    private AppCompatButton btn_driver;
 
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
         ListPagerAdapter adapter = new ListPagerAdapter(this);
-        adapter.addFragment(new IntroFragment(getLayoutInflater().inflate(R.layout.intro_1,null)));
-        adapter.addFragment(new IntroFragment(getLayoutInflater().inflate(R.layout.intro_2,null)));
+        adapter.addFragment(new IntroFragment(getLayoutInflater().inflate(R.layout.intro_1, null)));
+        adapter.addFragment(new IntroFragment(getLayoutInflater().inflate(R.layout.intro_2, null)));
 
         ViewPager2 vp = findViewById(R.id.viewpager);
         vp.setAdapter(adapter);
 
-        AlphaAnimation hideA = new AlphaAnimation(1.0f, 0.0f);
-        hideA.setDuration(150);
-        hideA.setFillAfter(true);
-        AlphaAnimation showA = new AlphaAnimation(0.0f, 1.0f);
-        showA.setDuration(150);
-        showA.setFillAfter(true);
 
-        AlphaAnimation hideB = new AlphaAnimation(1.0f, 0.0f);
-        hideB.setDuration(150);
-        hideB.setFillAfter(true);
-        AlphaAnimation showB = new AlphaAnimation(0.0f, 1.0f);
-        showB.setDuration(150);
-        showB.setFillAfter(true);
+        prevB = findViewById(R.id.prevB);
+        nextB = findViewById(R.id.nextB);
+        btn_driver = findViewById(R.id.btn_driver);
+        btn_passenger = findViewById(R.id.btn_passenger);
 
-        AlphaAnimation hideC = new AlphaAnimation(1.0f, 0.0f);
-        hideC.setDuration(150);
-        hideC.setFillAfter(true);
-        AlphaAnimation showC = new AlphaAnimation(0.0f, 1.0f);
-        showC.setDuration(150);
-        showC.setFillAfter(true);
+        vp.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                if (position == 0) {
+                    prevB.setVisibility(View.INVISIBLE);
+                    btn_passenger.setVisibility(View.INVISIBLE);
+                    nextB.setVisibility(View.VISIBLE);
+                    btn_driver.setVisibility(View.VISIBLE);
+                } else if (position == 1) {
+                    nextB.setVisibility(View.INVISIBLE);
+                    btn_driver.setVisibility(View.INVISIBLE);
+                    prevB.setVisibility(View.VISIBLE);
+                    btn_passenger.setVisibility(View.VISIBLE);
+
+                }
+            }
+
+        });
+
+        prevB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vp.getCurrentItem() != 0)
+                    vp.setCurrentItem(vp.getCurrentItem() - 1);
+            }
+
+        });
+
+        nextB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vp.getCurrentItem() != 1)
+                    vp.setCurrentItem(vp.getCurrentItem() + 1);
+            }
+        });
+
+        btn_passenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,PhonePassenger.class));
+                finish();
+            }
+        });
+
+        btn_driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this,PhoneDriver.class));
+                finish();
+            }
+        });
+
 
     }
 
